c9b7f5e8ccfae243b0ef99609f18595a4b483f34
liaten <vlad171vlad@gmail.com>
2022-08-03 20:19:08 +0300

    Заголовки, переходы и всё такое

---
+++ app/src/main/java/komi/republic/marshakovka/LauncherActivity.kt
@@ -0,0 +1,66 @@
+package komi.republic.marshakovka
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class LauncherActivity : AppCompatActivity() {
+
+    private var bottomNavigationView: BottomNavigationView? = null
+    private val TAG = "LauncherActivity"
+    private var backWasPressed: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.launcher)
+        setViews()
+        setListeners()
+        showMainPage()
+    }
+
+    override fun onBackPressed() {
+        if (backWasPressed) {
+            super.onBackPressed()
+            moveTaskToBack(true)
+            backWasPressed = false
+            finish()
+        } else {
+            Toast.makeText(
+                this,
+                getString(R.string.close_on_back_alert),
+                Toast.LENGTH_SHORT
+            ).show()
+            backWasPressed = true
+        }
+    }
+
+    private fun showMainPage(){
+        val menu: Menu = bottomNavigationView!!.menu
+        val menuItem:MenuItem = menu.getItem(0)
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container,
+            CustomFragment.newInstance(menuItem)
+        ).commit()
+    }
+
+    private fun setViews() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+    }
+
+    private fun setListeners() {
+        bottomNavigationView?.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container,
+                CustomFragment.newInstance(item)
+            ).commit()
+            return@OnNavigationItemSelectedListener true
+        }
+}
